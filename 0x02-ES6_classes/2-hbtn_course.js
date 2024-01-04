b94b5716f72ccf0fc90626d7fbe021a060732681
Hilly-man <hilkiahe@gmail.com>
2024-01-04 23:04:07 +0000

    Task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,55 @@
+class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = this.validateString(name, 'name');
+    this._length = this.validateNumber(length, 'length');
+    this._students = this.validateArray(students, 'students');
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = this.validateString(newName, 'name');
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    this._length = this.validateNumber(newLength, 'length');
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    this._students = this.validateArray(newStudents, 'students');
+  }
+
+  validateString(value, attribute) {
+    if (typeof value !== 'string') {
+      throw new TypeError(`${attribute} must be a string`);
+    }
+    return value;
+  }
+
+  validateNumber(value, attribute) {
+    const parsedValue = parseFloat(value);
+    if (isNaN(parsedValue) || !Number.isInteger(parsedValue) || parsedValue < 0) {
+      throw new TypeError(`${attribute} must be a valid positive integer`);
+    }
+    return parsedValue;
+  }
+
+  validateArray(value, attribute) {
+    if (!Array.isArray(value)) {
+      throw new TypeError(`${attribute} must be an array`);
+    }
+    return value;
+  }
+}
+
+export default HolbertonCourse;

efff8ab363ba1c76dcdd53ed9bacdfc77e8cf0f8
Hilly-man <hilkiahe@gamil.com>
2024-03-29 07:58:15 +0000

    done

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,17 +1,22 @@
 function calculateNumber(type, a, b) {
-  a = Math.round(a);
-  b = Math.round(b);
+  const num1 = Number(a);
+  const num2 = Number(b);
 
-  switch (type) {
-    case 'SUM':
-      return a + b;
-    case 'SUBTRACT':
-      return a - b;
-    case 'DIVIDE':
-      return b !== 0 ? a / b : 'Error';
-    default:
-      return 'Error';
+  if (Number.isNaN(num1) || Number.isNaN(num2)) {
+    throw TypeError;
+  }
+  
+  if (type === 'SUM') {
+    return (Math.round(num1) + Math.round(num2));
+  } else if(type === 'SUBTRACT') {
+    return (Math.round(num1) - Math.round(num2));
+  } else if (type === 'DIVIDE') {
+    if (Math.round(num2) === 0) {
+      return ('Error');
+    }
+    return (Math.round(num1) / Math.round(num2));
+  } else {
+    throw TypeError;
   }
 }
-
 module.exports = calculateNumber;
